eac9c1194597eecb839487a1b8b831b434c615dd
Juan Silva <juanse909@hotmail.com>
2025-05-08 10:54:01 -0500

    feat: Update socket services

--- server/services/socket.service.js
+++ server/services/socket.service.js
@@ -1,4 +1,6 @@
+// server/services/socket.service.js
 const { Server } = require("socket.io");
+const playersDb = require("../db/players.db");
 
 let io;
 
@@ -8,6 +10,34 @@ const initSocketInstance = (httpServer) => {
     cors: {
       origin: "*",
     },
+  });
+
+  // Set up event handlers for socket connections
+  io.on("connection", (socket) => {
+    console.log(`New client connected: ${socket.id}`);
+
+    // Handle player requesting current players list (for results screen)
+    socket.on("getPlayers", () => {
+      const players = playersDb.getAllPlayers();
+      socket.emit("playersUpdate", players);
+    });
+
+    // Handle game reset request from results screen
+    socket.on("resetGame", () => {
+      playersDb.resetPlayerScores();
+      const players = playersDb.getAllPlayers();
+
+      // Notify all clients about the updated scores
+      io.emit("playersUpdate", players);
+
+      // Notify all clients to reset the game
+      io.emit("gameReset");
+    });
+
+    // Handle client disconnection
+    socket.on("disconnect", () => {
+      console.log(`Client disconnected: ${socket.id}`);
+    });
   });
 };
 

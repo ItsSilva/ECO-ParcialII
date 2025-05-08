d309d44671bbb841055c4d98002fa722ccb654a7
Juan Silva <juanse909@hotmail.com>
2025-05-08 10:39:28 -0500

    feat: Update Game Router

--- server/routes/game.router.js
+++ server/routes/game.router.js
@@ -1,12 +1,14 @@
-const express = require('express');
-const gameController = require('../controllers/game.controller');
+// server/routes/game.router.js
+const express = require("express");
+const gameController = require("../controllers/game.controller");
 const router = express.Router();
 
 // Define game-related routes and link them to controller methods
-router.post('/join', gameController.joinGame);
-router.post('/start', gameController.startGame);
-router.post('/marco', gameController.notifyMarco);
-router.post('/polo', gameController.notifyPolo);
-router.post('/select-polo', gameController.selectPolo);
+router.post("/join", gameController.joinGame);
+router.post("/start", gameController.startGame);
+router.post("/marco", gameController.notifyMarco);
+router.post("/polo", gameController.notifyPolo);
+router.post("/select-polo", gameController.selectPolo);
+router.post("/reset-scores", gameController.resetScores);
 
 module.exports = router;
